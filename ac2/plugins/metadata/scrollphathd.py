7c010d2da1a063f3e10e85865a92f52eaa63a241
Bert <bert.marcelis@outlook.com>
2020-10-31 16:56:36 +0100

    Initial work to support the Pimoroni ScrollPhatHd as a metadata display

--- ac2/plugins/metadata/scrollphathd.py
+++ ac2/plugins/metadata/scrollphathd.py
@@ -14,6 +14,7 @@ class MetadataScrollPhatHdDisplay(MetadataDisplay):
 
     def __init__(self, display_player=True, display_artist=True, display_title=True, brightness=0.5):
         super().__init__()
+        self.current_metadata = None
         self.display_thread = ScrollPhatHdDisplayThread(display_player, display_artist, display_title, brightness)
         self.display_thread.start()
 
@@ -22,11 +23,16 @@ class MetadataScrollPhatHdDisplay(MetadataDisplay):
             self.display_thread.exit()
 
     def notify(self, metadata):
-        print(metadata)
+        if self.current_metadata is not None \
+                and metadata.playerName == self.current_metadata.playerName \
+                and metadata.artist == self.current_metadata.artist \
+                and metadata.title == self.current_metadata.title:
+            return
+        self.current_metadata = metadata
         self.display_thread.set_metadata(metadata)
 
     def __str__(self):
-        return "http"
+        return "MetadataScrollPhatHdDisplay"
 
 
 class ScrollPhatHdDisplayThread(threading.Thread):
@@ -54,6 +60,17 @@ class ScrollPhatHdDisplayThread(threading.Thread):
     def run(self):
         player_text = ""
         scrolling_text = ""
+        scrollphathd.fill(brightness=self.brightness)
+        scrollphathd.show()
+        time.sleep(0.5)
+        scrollphathd.clear()
+        scrollphathd.show()
+        scrollphathd.write_string("READY!", y=1, font=font3x5)
+        scrollphathd.show()
+        time.sleep(1)
+        scrollphathd.clear()
+        scrollphathd.show()
+
         logging.info("SCROLLPHAT DISPLAY THREAD START")
         # Auto scroll using a while + time mechanism (no thread)
         while self.active:
@@ -76,8 +93,9 @@ class ScrollPhatHdDisplayThread(threading.Thread):
                     scrolling_text = scrolling_text.upper()
                     self.metadata_update_pending = False
 
-                self.display_static(player_text, duration=10)
-                self.display_scrolling(scrolling_text, loops=2)
+                self.display_static(player_text, duration=5)
+                self.display_scrolling(scrolling_text)
+                self.display_scrolling(scrolling_text)
             except Exception as e:
                 logging.error("SCROLLPHAT DISPLAY THREAD EXCEPTION ", e)
         logging.info("SCROLLPHAT DISPLAY THREAD DONE")
@@ -91,33 +109,32 @@ class ScrollPhatHdDisplayThread(threading.Thread):
         scrollphathd.show()
         time.sleep(duration)
         scrollphathd.clear()
+        scrollphathd.show()
 
-    def display_scrolling(self, scrolling_text, loops=1):
+    def display_scrolling(self, scrolling_text):
         if not scrolling_text:
             return
 
-        display_text = scrolling_text
-        # Loop the text a number of times
-        if loops > 1:
-            for i in range(1, loops):
-                display_text += "    " + scrolling_text
-
         scrollphathd.write_string(scrolling_text, y=1, font=font3x5, brightness=self.brightness)
+        scrollphathd.show()
+        time.sleep(0.1)
 
-        # The font is 5x7. scrollphathd.scroll() moves all text one pixel
-        # The total text length is 5 x length(scrolling_text)
+        # The font is 3x5 with one column as a space behind every character.
+        # scrollphathd.scroll() moves all text one pixel
+        # The total text length is 4 x length(scrolling_text), including spaces
         # The first 17 columns fit on the display without scrolling
-        # So for each loop, call scroll '5 x length(scrolling_text) - 17' times
+        # The last space should not be scrolled
         # This means that, in order to scroll one loop, we need to scroll
-        scroll_calls = 5 * len(scrolling_text) - 17
+        scroll_calls = 4 * len(scrolling_text) - 17 - 1
         if scroll_calls < 0:
             scroll_calls = 0
 
-        scrollphathd.show()
         for i in range(scroll_calls):
             scrollphathd.scroll()
+            scrollphathd.show()
             time.sleep(0.1)
         # Sleep two seconds after the loop for better readability
         time.sleep(2)
 
         scrollphathd.clear()
+        scrollphathd.show()
