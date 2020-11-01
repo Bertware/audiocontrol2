d7e8dc3a2abfdc212649af1fe3b60398341d2920
Bert <bert.marcelis@outlook.com>
2020-11-01 21:33:43 +0100

    Add detection for the microdotphat, small improvements to prevent flickering, code quality improvements

--- ac2/plugins/metadata/microdotphat.py
+++ ac2/plugins/metadata/microdotphat.py
@@ -4,6 +4,7 @@ import microdotphat
 import time
 import threading
 import logging
+from smbus import SMBus
 
 
 class MetadataMicroDotPhatDisplay(MetadataDisplay):
@@ -13,6 +14,12 @@ class MetadataMicroDotPhatDisplay(MetadataDisplay):
 
     def __init__(self, display_player=True, display_artist=True, display_title=True, brightness=0.5):
         super().__init__()
+        self.enabled = self.is_hardware_present()
+        # Only enable this plugin if the microdot pHAT hardware is connected
+        if not self.enabled:
+            logging.warning("MicroDotPhat not detected, microdotphat display module disabled")
+            return
+        logging.info("MicroDotPhat detected, enabling display output")
         self.current_metadata = None
         self.display_thread = MicroDotPhatDisplayThread(display_player, display_artist, display_title, brightness)
         self.display_thread.start()
@@ -22,6 +29,9 @@ class MetadataMicroDotPhatDisplay(MetadataDisplay):
             self.display_thread.exit()
 
     def notify(self, metadata):
+        # Only enable this plugin if the microdot pHAT hardware is connected
+        if not self.enabled:
+            return
         if self.current_metadata is not None \
                 and metadata.playerState == self.current_metadata.playerState \
                 and metadata.playerName == self.current_metadata.playerName \
@@ -31,8 +41,20 @@ class MetadataMicroDotPhatDisplay(MetadataDisplay):
         self.current_metadata = metadata
         self.display_thread.set_metadata(metadata)
 
+    @staticmethod
+    def is_hardware_present():
+        bus = SMBus(1)
+        try:
+            # The microdot pHat uses 3 IS31FL3730 chips located at 0x61, 0x62 and 0x63
+            bus.write_byte(0x61, 0)
+            bus.write_byte(0x62, 0)
+            bus.write_byte(0x63, 0)
+            return True
+        except:  # exception if read_byte fails, meaning the device isn't connected
+            return False
+
     def __str__(self):
-        return "MetadataMicroDotPhatDisplay"
+        return "MetadataMicroDotPhatDisplay " + "(enabled)" if self.enabled else "(disabled)"
 
 
 class MicroDotPhatDisplayThread(threading.Thread):
@@ -48,6 +70,7 @@ class MicroDotPhatDisplayThread(threading.Thread):
 
     def __del__(self):
         microdotphat.clear()
+        microdotphat.show()
 
     def set_metadata(self, metadata):
         self.metadata = metadata
@@ -65,10 +88,11 @@ class MicroDotPhatDisplayThread(threading.Thread):
         microdotphat.fill(1)
         microdotphat.show()
         time.sleep(0.5)
-        microdotphat.clear()
-        microdotphat.write_string("READY!", kerning=False)
-        microdotphat.show()
-        time.sleep(1)
+        for word in ("READY", "TO", "PLAY"):
+            microdotphat.clear()
+            microdotphat.write_string(word, kerning=False)
+            microdotphat.show()
+            time.sleep(1)
         microdotphat.clear()
         microdotphat.show()
 
@@ -118,15 +142,10 @@ class MicroDotPhatDisplayThread(threading.Thread):
         microdotphat.show()
         time.sleep(duration)
         microdotphat.clear()
-        microdotphat.show()
 
     def display_scrolling(self, scrolling_text):
         if not scrolling_text:
             return
-
-        microdotphat.clear()
-        microdotphat.set_brightness(self.brightness)
-        microdotphat.write_string(scrolling_text, kerning=False)
 
         # The microdotphat has 6 character displays. microdotphat.scroll() moves all text one character
         # The total text length is length(scrolling_text)
@@ -137,9 +156,8 @@ class MicroDotPhatDisplayThread(threading.Thread):
         if scroll_calls < 0:
             scroll_calls = 0
 
-        microdotphat.show()
         # + 1 since we also need to show offset 0 for the initial text
-        for i in range(scroll_calls+1):
+        for i in range(scroll_calls + 1):
             microdotphat.clear()
             # Stop showing the current text if there is new data available
             if self.metadata_update_pending:
@@ -147,9 +165,11 @@ class MicroDotPhatDisplayThread(threading.Thread):
                 return
             microdotphat.write_string(scrolling_text[i:], kerning=False)
             microdotphat.show()
-            time.sleep(0.6)
+            if i == 0:
+                time.sleep(6 * 0.3)
+            else:
+                time.sleep(0.3)
         # Sleep two seconds after the loop for better readability
-        time.sleep(2)
+        time.sleep(1.8)
 
         microdotphat.clear()
-        microdotphat.show()
