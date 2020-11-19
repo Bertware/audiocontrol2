270396537220be9b6ecd54e6d5b9d6aea90459d5
Bert <bert.marcelis@outlook.com>
2020-11-19 22:06:42 +0100

    Add volume level display for microdotphat

--- ac2/plugins/metadata/microdotphat.py
+++ ac2/plugins/metadata/microdotphat.py
@@ -1,5 +1,5 @@
 from ac2.plugins.metadata import MetadataDisplay
-
+from ac2.metadata import Metadata
 import microdotphat
 import time
 import threading
@@ -9,7 +9,7 @@ from smbus import SMBus
 from json import dumps
 
 
-class MetadataMicroDotPhatDisplay(MetadataDisplay):
+class MicroDotPhatMetadataDisplay(MetadataDisplay):
     '''
     Show metadata on a Pimoroni Micro Dot pHat 6 character display where each character is 8x5 pixels
     '''
@@ -23,11 +23,16 @@ class MetadataMicroDotPhatDisplay(MetadataDisplay):
             return
         logging.info("MicroDotPhat detected, enabling display output")
         self.current_metadata = None
-        self.display_thread_comm_queue = SimpleQueue()
-        self.display_thread = MicroDotPhatDisplayThread(self.display_thread_comm_queue, True, True, True, 0.5)
+        self.display_thread_metadata_queue = SimpleQueue()
+        self.display_thread_volume_queue = SimpleQueue()
+        self.display_thread = MicroDotPhatDisplayThread(self.display_thread_metadata_queue,
+                                                        self.display_thread_volume_queue,
+                                                        True, True, True, 0.5)
         self.display_thread.start()
 
     def __del__(self):
+        self.display_thread_metadata_queue.put(None)
+        self.display_thread_volume_queue.put(None)
         if self.display_thread:
             self.display_thread.exit()
 
@@ -35,7 +40,6 @@ class MetadataMicroDotPhatDisplay(MetadataDisplay):
         # Only enable this plugin if the microdot pHAT hardware is connected
         if not self.enabled:
             return
-        logging.critical(metadata)
         if self.current_metadata is not None \
                 and metadata.playerState == self.current_metadata.playerState \
                 and metadata.playerName == self.current_metadata.playerName \
@@ -43,27 +47,32 @@ class MetadataMicroDotPhatDisplay(MetadataDisplay):
                 and metadata.title == self.current_metadata.title:
             logging.critical(self.current_metadata)
             return
-        self.display_thread_comm_queue.put(metadata)
+        self.display_thread_metadata_queue.put(metadata)
         self.current_metadata = metadata
+
+    def update_volume(self, volume_level):
+        # volume change
+        self.display_thread_volume_queue.put(volume_level)
 
     @staticmethod
     def is_hardware_present():
         return microdotphat.is_connected()
 
     def __str__(self):
-        return "MetadataMicroDotPhatDisplay " + "(enabled)" if self.enabled else "(disabled)"
+        return "MicroDotPhatMetadataDisplay " + "(enabled)" if self.enabled else "(disabled)"
 
 
 class MicroDotPhatDisplayThread(threading.Thread):
-    def __init__(self, communication_queue, display_playername, display_artist, display_title, brightness):
+    def __init__(self, metadata_queue, volume_queue, display_playername, display_artist, display_title, brightness):
         threading.Thread.__init__(self)
-        self.comm_queue = communication_queue
+        self.metadata_queue = metadata_queue
+        self.volume_queue = volume_queue
         self.display_player = display_playername
         self.display_artist = display_artist
         self.display_title = display_title
         self.brightness = brightness
+        self.volume_level = 0
         self.metadata = None
-        self.metadata_update_pending = False
         self.active = True
 
     def __del__(self):
@@ -71,15 +80,28 @@ class MicroDotPhatDisplayThread(threading.Thread):
         microdotphat.show()
 
     def is_metadata_update_pending(self):
-        return not self.comm_queue.empty()
+        return not self.metadata_queue.empty()
 
-    def read_queue(self):
-        if self.comm_queue.empty():
+    def is_volume_update_pending(self):
+        return not self.volume_queue.empty()
+
+    def is_update_pending(self):
+        return self.is_volume_update_pending() or self.is_metadata_update_pending()
+
+    def read_metadata_queue(self):
+        if self.metadata_queue.empty():
             return
-        while not self.comm_queue.empty():
-            self.metadata = self.comm_queue.get()
-        self.metadata_update_pending = True
+        while not self.metadata_queue.empty():
+            self.metadata = self.metadata_queue.get()
         if self.metadata is None:
+            self.active = False
+
+    def read_volume_queue(self):
+        if self.volume_queue.empty():
+            return
+        while not self.volume_queue.empty():
+            self.volume_level = self.volume_queue.get()
+        if self.volume_level is None:
             self.active = False
 
     def exit(self):
@@ -102,7 +124,7 @@ class MicroDotPhatDisplayThread(threading.Thread):
         while self.active:
             try:
                 if self.is_metadata_update_pending():
-                    self.read_queue()
+                    self.read_metadata_queue()
                     scrolling_text = ""
                     player_text = ""
 
@@ -118,11 +140,16 @@ class MicroDotPhatDisplayThread(threading.Thread):
                     player_text = player_text.upper()
                     scrolling_text = scrolling_text.upper()
 
+                if self.is_volume_update_pending():
+                    self.read_volume_queue()
+                    self.display_static(f"VOL{self.volume_level: 3}", duration=3)
+                    continue
+
                 # Empty display when not playing
                 if not self.metadata or self.metadata.playerState != "playing":
                     microdotphat.clear()
                     microdotphat.show()
-                    time.sleep(1)
+                    self.aware_sleep(1)
                     continue
 
                 if not scrolling_text:
@@ -137,7 +164,7 @@ class MicroDotPhatDisplayThread(threading.Thread):
         microdotphat.show()
 
     def display_static(self, static_text, duration=10):
-        if not static_text or self.is_metadata_update_pending():
+        if not static_text or self.is_update_pending():
             return
         microdotphat.clear()
         microdotphat.set_brightness(self.brightness)
@@ -145,14 +172,10 @@ class MicroDotPhatDisplayThread(threading.Thread):
         microdotphat.show()
         # Clear the buffer already, but leave the text on the display
         microdotphat.clear()
-        for i in range(duration):
-            # This makes the display more responsive to metadata changes
-            if self.is_metadata_update_pending():
-                return
-            time.sleep(1)
+        self.aware_sleep(duration)
 
     def display_scrolling(self, scrolling_text):
-        if not scrolling_text or self.is_metadata_update_pending():
+        if not scrolling_text or self.is_update_pending():
             return
 
         # The microdotphat has 6 character displays. microdotphat.scroll() moves all text one character
@@ -168,16 +191,25 @@ class MicroDotPhatDisplayThread(threading.Thread):
         for i in range(scroll_calls + 1):
             microdotphat.clear()
             # Stop showing the current text if there is new data available
-            if self.is_metadata_update_pending():
+            if self.is_update_pending():
                 microdotphat.show()
                 return
             microdotphat.write_string(scrolling_text[i:], kerning=False)
             microdotphat.show()
             if i == 0:
-                time.sleep(6 * 0.3)
+                self.aware_sleep(6 * 0.3)
             else:
-                time.sleep(0.3)
+                self.aware_sleep(0.3)
         # Sleep two seconds after the loop for better readability
-        if not self.is_metadata_update_pending():
-            time.sleep(1.8)
+        self.aware_sleep(1.8)
         microdotphat.clear()
+
+    def aware_sleep(self, duration_seconds : float):
+        '''
+        Sleep, but skip as soon as there is a change in the data.
+        '''
+        for i in range(round(duration_seconds * 10)):
+            if self.is_update_pending():
+                return
+            time.sleep(0.1)
+        return
